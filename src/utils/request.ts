0561e15285167e68c690e0f037e59b8f88b497c0
Upsiloner <1458335652@qq.com>
2025-09-01 19:49:28 +0800

    chore: add axios encapsulation

---
+++ src/utils/request.ts
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+const request = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000
+})
+
+// 请求拦截器
+request.interceptors.request.use(config => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// 响应拦截器
+request.interceptors.response.use(
+  response => response.data,
+  error => {
+    console.error("请求错误:", error)
+    return Promise.reject(error)
+  }
+)
+
+export default request
