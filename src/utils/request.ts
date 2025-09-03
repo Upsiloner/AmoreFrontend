5a4581d8fcdbd4da3813b54a6fa08535a05d0cec
Upsiloner <1458335652@qq.com>
2025-09-03 23:02:35 +0800

    feat: add error handling for response interceptors

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { message } from 'ant-design-vue'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -18,7 +19,27 @@ request.interceptors.request.use(config => {
 request.interceptors.response.use(
   response => response.data,
   error => {
-    console.error("请求错误:", error)
+    if (error.response) {
+      const status = error.response.status
+      switch (status) {
+        case 400:
+          message.error('[400] 请求参数错误')
+          break
+        case 401:
+          message.error('[401] 未授权，请重新登录')
+          break
+        case 404:
+          message.error('[404] 接口不存在')
+          break
+        case 500:
+          message.error('[500] 服务器错误，请稍后再试')
+          break
+        default:
+          message.error(`${status} 请求失败`)
+      }
+    } else {
+      message.error('网络异常，请检查网络连接')
+    }
     return Promise.reject(error)
   }
 )
